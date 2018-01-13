52b989e669ea52e55a2a9b0e63aa33c31b514af4
Christopher Hinton <cphinton42@gmail.com>
2018-01-12 19:15:02 -0600

    Moved things around (for not much reason). Implemented dynamically growing array

--- src/basic.h
+++ src/basic.h
@@ -35,6 +35,36 @@ struct Array
 
 typedef Array<byte> String;
 
+#define global static
+#define internal static
+
+
+#define zero_memory(ptr, size) \
+zero_memory_((ptr), (size)*sizeof(decltype(*(ptr))))
+#define zero_struct(ptr) \
+zero_memory_((ptr), sizeof(decltype(*(ptr))))
+#define copy_memory(dest, src, count) \
+copy_memory_((dest), (src), (count)*sizeof(decltype(*(dest))))
+#define fill_memory(ptr, val, count) \
+fill_memory_((ptr), (val), (count)*sizeof(decltype(*(ptr))))
+
+internal inline void zero_memory_(void *ptr, u64 size);
+internal inline void copy_memory_(void *dest, void *src, u64 size);
+internal inline void fill_memory_(void *dest, u8 value, u64 size);
+
+// Allocation functions (to be replaced ?)
+#define mem_alloc(type, n) \
+((type*)mem_alloc_((n)*sizeof(type)))
+#define mem_resize(old_ptr, old_n, new_n) \
+((decltype(old_ptr))mem_resize_((old_ptr), (old_n)*sizeof(decltype(*(old_ptr))), (new_n)*sizeof(decltype(*(old_ptr)))))
+#define mem_dealloc(ptr, n) \
+(mem_dealloc_(ptr, (n)*sizeof(decltype(*(ptr)))))
+
+internal inline void *mem_alloc_(u64 size);
+internal inline void *mem_realloc_(void *old_ptr, u64 old_size, u64 new_size);
+internal inline void mem_dealloc_(void *ptr, u64 size);
+
+
 template <typename T>
 struct Dynamic_Array
 {
@@ -53,91 +83,13 @@ struct Dynamic_Array
 };
 
 template<typename T>
-bool operator==(Array<T> a1, Array<T> a2)
-{
-    if(a1.count == a2.count)
-    {
-        for(u64 i = 0; i < a1.count; ++i)
-        {
-            if(a1[i] != a2[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+bool operator==(Array<T> a1, Array<T> a2);
 
 template<typename T>
-T &Array<T>::operator[](u64 idx)
-{
-    return this->data[idx];
-}
+Array<T> make_array(u64 count, T *data);
 
 template<typename T>
-T &Dynamic_Array<T>::operator[](u64 idx)
-{
-    return this->data[idx];
-}
-
-template<typename T>
-Array<T> make_array(u64 count, T *data)
-{
-    return {count, data};
-}
-
-template<typename T>
-void array_add(Dynamic_Array<T> *arr, T element)
-{
-    (*arr)[arr->count] = element;
-    ++arr->count;
-}
-
-template<typename T>
-void array_add(Dynamic_Array<T> *arr, T *element)
-{
-    (*arr)[arr->count] = *element;
-    ++arr->count;
-}
-
-
-#define global static
-#define internal static
-
-#define zero_memory(ptr, size) \
-zero_memory_((ptr), (size)*sizeof(decltype(*(ptr))))
-#define zero_struct(ptr) \
-zero_memory_((ptr), sizeof(decltype(*(ptr))))
-#define copy_memory(dest, src, count) \
-copy_memory_((dest), (src), (count)*sizeof(decltype(*(dest))))
-#define fill_memory(ptr, val, count) \
-fill_memory_((ptr), (val), (count)*sizeof(decltype(*(ptr))))
-
-
-internal inline void zero_memory_(void *ptr, u64 size);
-internal inline void copy_memory_(void *dest, void *src, u64 size);
-internal inline void fill_memory_(void *dest, u8 value, u64 size);
-
-
-//
-// Allocation functions (to be replaced later)
-//
-
-#define mem_alloc(type, n) \
-((type*)mem_alloc_((n)*sizeof(type)))
-#define mem_resize(old_ptr, old_n, new_n) \
-((decltype(old_ptr))mem_resize_((old_ptr), (old_n)*sizeof(decltype(*(old_ptr))), (new_n)*sizeof(decltype(*(old_ptr)))))
-#define mem_dealloc(ptr, n) \
-(mem_dealloc_(ptr, (n)*sizeof(decltype(*(ptr)))))
-
-internal inline void *mem_alloc_(u64 size);
-internal inline void *mem_realloc_(void *old_ptr, u64 old_size, u64 new_size);
-internal inline void mem_dealloc_(void *ptr, u64 size);
-
+void array_add(Dynamic_Array<T> *arr, T element);
 
 //
 // Common Macros
@@ -165,7 +117,7 @@ CONCAT(str, __LINE__)
 
 
 //
-// Inline functions
+// Inline and template function implementations
 //
 
 #include <stdlib.h>
@@ -195,6 +147,76 @@ internal inline void *mem_resize_(void *old_ptr, u64 old_size, u64 new_size)
 internal inline void mem_dealloc_(void *ptr, u64 size)
 {
     free(ptr);
+}
+
+template<typename T>
+T &Array<T>::operator[](u64 idx)
+{
+    return this->data[idx];
+}
+
+template<typename T>
+T &Dynamic_Array<T>::operator[](u64 idx)
+{
+    return this->data[idx];
+}
+
+template<typename T>
+bool operator==(Array<T> a1, Array<T> a2)
+{
+    if(a1.count == a2.count)
+    {
+        for(u64 i = 0; i < a1.count; ++i)
+        {
+            if(a1[i] != a2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template<typename T>
+Array<T> make_array(u64 count, T *data)
+{
+    return {count, data};
+}
+
+template<typename T>
+void array_add(Dynamic_Array<T> *arr, T element)
+{
+    if(arr->count == arr->allocated)
+    {
+        // TODO: take more care with OOM errors?
+        if(arr->data)
+        {
+            u64 new_allocated = 4 * arr->allocated;
+            if(new_allocated == 0)
+            {
+                new_allocated = 4;
+            }
+            
+            arr->data = mem_resize(arr->data, arr->allocated, new_allocated);
+            assert(arr->data);
+            arr->allocated = new_allocated;
+        }
+        else
+        {
+            assert(arr->count == 0);
+            
+            arr->data = mem_alloc(T, 4);
+            assert(arr->data);
+            arr->allocated = 4;
+        }
+    }
+    
+    (*arr)[arr->count] = element;
+    ++arr->count;
 }
 
 #endif // BASIC_H
